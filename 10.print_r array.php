3004cdb5ceacc0adfb1275faf2c043d156cf6bd3
dewi22222222 <dewiksp972@gmail.com>
2024-08-29 11:40:49 +0700

    print_r array.php

---
+++ 10.print_r array.php
@@ -0,0 +1,22 @@
+<?php
+$array = array(
+    'name' => 'John Doe',
+    'age' => 16,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+print_r($array);
+
+print "<p>";
+
+$array = array(
+    'name' => 'John Doe',
+    'age' => 16,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+var_dump($array);
+
+print "<p>";
+
+echo "<br><br><strong>by dewi puspita sari";
