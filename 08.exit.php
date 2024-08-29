869df61f6d91e28c35bfc950951370c4649410a6
dewi22222222 <dewiksp972@gmail.com>
2024-08-29 11:05:56 +0700

    exit.php

---
+++ 08.exit.php
@@ -0,0 +1,11 @@
+<?php
+echo 'Ini akan tampil di browser.';
+exit('Proses telah selesai.');
+
+// Jika terjadi error
+exit(1);
+
+// Jika tidak ada error
+exit(0);
+
+echo "<br><br><strong>by dewi puspita sari";
