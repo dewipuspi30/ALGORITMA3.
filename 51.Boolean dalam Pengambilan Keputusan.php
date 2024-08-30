6de83732a27af699800d9f3d7e543e0d448590b5
dewi22222222 <dewiksp972@gmail.com>
2024-08-30 10:59:39 +0700

    Boolean dalam Pengambilan Keputusan.php

---
+++ 51.Boolean dalam Pengambilan Keputusan.php
@@ -0,0 +1,10 @@
+<?php
+$login_sukses = true;
+
+if ($login_sukses) {
+    echo "Selamat datang pengguna!";
+} else {
+    echo "Login gagal, silakan coba lagi.";
+}
+
+echo"<br><br><strong>by dewi puspita sari";
