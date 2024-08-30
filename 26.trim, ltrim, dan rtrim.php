a2d27f443b2eb10076d34226c7209de0642e923a
dewi22222222 <dewiksp972@gmail.com>
2024-08-30 09:01:27 +0700

    trim,Itrim,dan rtrim.php

---
+++ 26.trim, ltrim, dan rtrim.php
@@ -0,0 +1,7 @@
+<?php
+$text = " Hello World! ";
+echo "[" . trim($text) . "]"; // Outputs: "[Hello World!]"
+echo "[" . ltrim($text) . "]"; // Outputs: "[Hello World! ]"
+echo "[" . rtrim($text) . "]"; // Outputs: "[ Hello World!]"
+
+echo"<br><br><strong>by dewi puspita sari";
