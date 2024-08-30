f081ba771714086dfab5028d4955662170912b86
dewi22222222 <dewiksp972@gmail.com>
2024-08-30 11:06:23 +0700

    Penulisan Sintaks If.php

---
+++ 54.Penulisan Sintaks If.php
@@ -0,0 +1,12 @@
+<?php
+if (kondisi) {
+    // kode yang akan dijalankan jika kondisi bernilai true
+}
+Penulisan Sintaks Else
+if (kondisi) {
+    // kode yang akan dijalankan jika kondisi bernilai true
+} else {
+    // kode yang akan dijalankan jika kondisi bernilai false
+}
+
+echo"<br><br><strong>by dewi puspita sari";
