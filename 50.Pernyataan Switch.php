9da2aa5728b9c813215ef4e9777b048c33dda37d
dewi22222222 <dewiksp972@gmail.com>
2024-08-30 10:57:17 +0700

    pernyataan switch.php

---
+++ 50.Pernyataan Switch.php
@@ -0,0 +1,14 @@
+<?php
+$grade = 'B';
+switch ($grade) {
+  case 'A':
+    echo "Luar biasa!";
+    break;
+  case 'B':
+  case 'C':
+    echo "Bagus!";
+    break;
+  default:
+    echo "Tetap semangat!";
+}
+echo"<br><br><strong>by dewi puspita sari";
